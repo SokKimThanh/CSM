8f6a9ceba2873ccd71d326b025481ca5b1e0cf69
SokKimThanh <thanhsk1991@gmail.com>
2024-06-16 10:34:07 +0700

    add new solution

---
+++ CSM_Data_Access/CUsing_Global.cs
@@ -0,0 +1,10 @@
+﻿//Tại đây chỉ khai báo các using sử dụng chung và lặp lại nhiều lần
+
+global using CSM_Data_Access.Utility;
+global using Microsoft.Data.SqlClient;
+global using Newtonsoft.Json;
+global using OfficeOpenXml;
+global using System.Data;
+global using System.Globalization;
+global using System.Reflection;
+global using System.Text;
---
+++ CSM_Web/Thread/CCache_Timer_Service.cs
@@ -0,0 +1,19 @@
+﻿
+namespace CSM_Web.Thread
+{
+    public class CCache_Timer_Service : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                //Tại đây gọi các hàm load cache
+
+
+
+                // Chờ 1 giây trước khi thực hiện lại
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
